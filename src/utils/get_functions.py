56aaeefee53a8ce5659cd750362298e169826e24
Marco Mungai Coppolino <marcomungaicoppolino@gmail.com>
2024-05-27 16:55:23 +0200

    Update get_functions.py

--- src/utils/get_functions.py
+++ src/utils/get_functions.py
@@ -9,32 +9,28 @@ def get_dataset(args):
     """
 
     if args.dataset == 'cifar':
-        data_dir = '../data'
+        data_dir = './data'
         apply_transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
         train_dataset = datasets.CIFAR100(data_dir, train=True, download=True,
                                        transform=apply_transform)
-        #TODO: @Marco: update the dataset to CIFAR100
-        train_set, val_set = stratified_split(train_dataset, val_split=0.2)
-
 
         test_dataset = datasets.CIFAR100(data_dir, train=False, download=True,
                                       transform=apply_transform)
 
         # sample training data amongst users
         if args.iid:
-            # Sample IID user data from cifar
-            user_groups = cifar_iid(train_set, args.num_users)
+            # Sample IID user data from Mnist
+            user_groups_train, user_groups_val, user_groups_test = cifar_iid(train_dataset, args.num_users)
         else:
-            user_groups = cifar_noniid(train_set, args.num_users, args.Nc)
+            user_groups_train, user_groups_val, user_groups_test = cifar_noniid(train_dataset, args.num_users, args.Nc)
 
     elif args.dataset == 'shakespears':
-        #TODO: implement the shakespears dataset
         print("ancora da finire")
 
-    return train_set, val_set, test_dataset, user_groups
+    return train_dataset, test_dataset, user_groups_train, user_groups_val, user_groups_test
 
 
 def get_user_input():
