069335fad8fa953638866c4ee8b3905219430dc9
Marco Mungai Coppolino <marcomungaicoppolino@gmail.com>
2024-05-27 16:53:13 +0200

    Update lenet5.py

--- src/models/cifar/lenet5.py
+++ src/models/cifar/lenet5.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LeNet5(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, args):
         super(LeNet5, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=5, padding=2)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=5, padding=2)
         self.fc1 = nn.Linear(64 * 8 * 8, 384)
         self.fc2 = nn.Linear(384, 192)
-        self.fc3 = nn.Linear(192, num_classes)
+        self.fc3 = nn.Linear(192, args.num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -18,8 +18,9 @@ class LeNet5(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        x = F.softmax(x, dim=1)  # Softmax layer for classification
-        return x
+        # x = F.softmax(x, dim=1)  # Softmax layer for classification
+        # return x
+        return F.log_softmax(x, dim=1)
     
 
 __all__ = ["LeNet5"]
