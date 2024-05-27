f7e8a99deac168edbd7548924a6ba17f43f27536
Marco Mungai Coppolino <marcomungaicoppolino@gmail.com>
2024-05-27 17:27:09 +0200

    Update exp_details.py

---
+++ src/utils/exp_details.py
@@ -0,0 +1,20 @@
+import argparse
+
+def exp_details(args):
+    print('\nExperimental details:')
+    print(f'    Model     : {args.model}')
+    print(f'    Optimizer : {args.optimizer}')
+    print(f'    Learning  : {args.lr}')
+    print(f'    Global Rounds   : {args.epochs}\n')
+
+    print('    Federated parameters:')
+    if args.iid:
+        print('    IID')
+    else:
+        print('    Non-IID')
+    print(f'    Fraction of users  : {args.frac}')
+    print(f'    Local Batch size   : {args.local_bs}')
+    print(f'    Local Epochs       : {args.local_ep}\n')
+    return
+
+__all__ = ['exp_details']
