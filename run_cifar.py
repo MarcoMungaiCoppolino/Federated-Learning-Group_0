8164be6a05331b46ae56cfd50bdb605e073dcd58
developer-sidani <developer.sidani@gmail.com>
2024-06-02 03:28:08 +0200

    fix: update the model

--- run_cifar.py
+++ run_cifar.py
@@ -21,6 +21,7 @@ def args_parser():
     # location argument
     parser.add_argument('--shakespeare_train_path', type=str, default='', help="training path for shakespeare dataset")
     parser.add_argument('--shakespeare_test_path', type=str, default='', help="test path for shakespeare dataset")
+    parser.add_argument('--cifar_dataset', type=str, default='./data/cifar', help="path for cifar dataset")
 
     # federated arguments
     parser.add_argument('--epochs', type=int, default=5, help="rounds of training")
@@ -30,7 +31,7 @@ def args_parser():
     parser.add_argument('--local_bs', type=int, default=100, help="local batch size: B")
     parser.add_argument('--bs', type=int, default=128, help="test batch size")
     parser.add_argument('--lr', type=float, default=0.01, help="learning rate")
-    parser.add_argument('--lr_decay', type=float, default=0.995, help="learning rate decay each round")
+    parser.add_argument('--lr_decay', type=float, default=4*10^-3, help="learning rate decay each round")
     parser.add_argument('--split', type=str, default='user', help="train-test split type, user or sample")
 
     # model arguments
@@ -74,52 +75,9 @@ if args.wandb_key:
   wandb.login(key=args.wandb_key)
 
     # Initialize wandb
-  wandb.init(project='fed-shakespeare', entity='developer-sidani')
+  wandb.init(project='fed-2', entity='developer-sidani')
 else:
     print("wandb key not provided, logging is disabled")
-
-"""Utils for language models."""
-
-import re
-import numpy as np
-import json
-
-
-# ------------------------
-# utils for shakespeare dataset
-
-ALL_LETTERS = "\n !\"&'(),-.0123456789:;>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[]abcdefghijklmnopqrstuvwxyz}"
-NUM_LETTERS = len(ALL_LETTERS)
-# print(NUM_LETTERS)
-
-def _one_hot(index, size):
-    '''returns one-hot vector with given size and value 1 at given index
-    '''
-    vec = [0 for _ in range(size)]
-    vec[int(index)] = 1
-    return vec
-
-
-def letter_to_vec(letter):
-    '''returns one-hot representation of given letter
-    '''
-    index = ALL_LETTERS.find(letter)
-    return index
-
-
-def word_to_indices(word):
-    '''returns a list of character indices
-
-    Args:
-        word: string
-    
-    Return:
-        indices: int list with length len(word)
-    '''
-    indices = []
-    for c in word:
-        indices.append(ALL_LETTERS.find(c))
-    return indices
 
 
 
@@ -129,175 +87,95 @@ from collections import defaultdict
 import numpy as np
 from torch.utils.data import Dataset
 import torch
+from torchvision import datasets, transforms
+
+def cifar_iid(dataset, num_users):
+    """
+    Sample I.I.D. client data from CIFAR10 dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    dict_users = {}
+    num_items = int(len(dataset) / num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+def cifar_noniid(dataset, num_users):
+    """
+    Sample non-I.I.D client data from CIFAR10 dataset
+    :param dataset:
+    :param num_users:
+    :return:
+    """
+    num_shards, num_imgs = num_users * 2, int(len(dataset) / (num_users * 2))
+    idx_shard = [i for i in range(num_shards)]
+    dict_users = {i: np.array([], dtype='int64') for i in range(num_users)}
+    idxs = np.arange(num_shards * num_imgs)
+    # labels = dataset.train_labels.numpy()
+    labels = np.array(dataset.targets)
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
+    # divide and assign
+    for i in range(num_users):
+        rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+        idx_shard = list(set(idx_shard) - rand_set)
+        for rand in rand_set:
+            dict_users[i] = np.concatenate((dict_users[i], idxs[rand * num_imgs:(rand + 1) * num_imgs]), axis=0)
+    return dict_users
 
 
-
-class ShakeSpeare(Dataset):
-    def __init__(self, train=True, args=None):
-        super(ShakeSpeare, self).__init__()
-        train_clients, train_groups, train_data_temp, test_data_temp = read_data(args.shakespeare_train_path, args.shakespeare_test_path)
-        self.train = train
-
-        if self.train:
-            self.dic_users = {}
-            train_data_x = []
-            train_data_y = []
-            for i in range(len(train_clients)):
-                # if i == 100:
-                #     break
-                self.dic_users[i] = set()
-                l = len(train_data_x)
-                cur_x = train_data_temp[train_clients[i]]['x']
-                cur_y = train_data_temp[train_clients[i]]['y']
-                for j in range(len(cur_x)):
-                    self.dic_users[i].add(j + l)
-                    train_data_x.append(cur_x[j])
-                    train_data_y.append(cur_y[j])
-            self.data = train_data_x
-            self.label = train_data_y
-        else:
-            test_data_x = []
-            test_data_y = []
-            for i in range(len(train_clients)):
-                cur_x = test_data_temp[train_clients[i]]['x']
-                cur_y = test_data_temp[train_clients[i]]['y']
-                for j in range(len(cur_x)):
-                    test_data_x.append(cur_x[j])
-                    test_data_y.append(cur_y[j])
-            self.data = test_data_x
-            self.label = test_data_y
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        sentence, target = self.data[index], self.label[index]
-        indices = word_to_indices(sentence)
-        target = letter_to_vec(target)
-        # y = indices[1:].append(target)
-        # target = indices[1:].append(target)
-        indices = torch.LongTensor(np.array(indices))
-        # y = torch.Tensor(np.array(y))
-        # target = torch.LongTensor(np.array(target))
-        return indices, target
-
-    def get_client_dic(self):
-        if self.train:
-            return self.dic_users
-        else:
-            exit("The test dataset do not have dic_users!")
-
-
-def batch_data(data, batch_size, seed):
-    '''
-    data is a dict := {'x': [numpy array], 'y': [numpy array]} (on one client)
-    returns x, y, which are both numpy array of length: batch_size
-    '''
-    data_x = data['x']
-    data_y = data['y']
-
-    # randomly shuffle data
-    np.random.seed(seed)
-    rng_state = np.random.get_state()
-    np.random.shuffle(data_x)
-    np.random.set_state(rng_state)
-    np.random.shuffle(data_y)
-
-    # loop through mini-batches
-    for i in range(0, len(data_x), batch_size):
-        batched_x = data_x[i:i + batch_size]
-        batched_y = data_y[i:i + batch_size]
-        yield (batched_x, batched_y)
-
-
-def read_dir(data_dir):
-    clients = []
-    groups = []
-    data = defaultdict(lambda: None)
-
-    files = os.listdir(data_dir)
-    files = [f for f in files if f.endswith('.json')]
-    for f in files:
-        file_path = os.path.join(data_dir, f)
-        with open(file_path, 'r') as inf:
-            cdata = json.load(inf)
-        clients.extend(cdata['users'])
-        if 'hierarchies' in cdata:
-            groups.extend(cdata['hierarchies'])
-        data.update(cdata['user_data'])
-
-    clients = list(sorted(data.keys()))
-    return clients, groups, data
-
-
-
-def read_data(train_data_dir, test_data_dir):
-    '''parses data in given train and test data directories
-
-    assumes:
-    - the data in the input directories are .json files with
-        keys 'users' and 'user_data'
-    - the set of train set users is the same as the set of test set users
-
-    Return:
-        clients: list of client ids
-        groups: list of group ids; empty list if none found
-        train_data: dictionary of train data
-        test_data: dictionary of test data
-    '''
-    train_clients, train_groups, train_data = read_dir(train_data_dir)
-    test_clients, test_groups, test_data = read_dir(test_data_dir)
-
-    assert train_clients == test_clients
-    assert train_groups == test_groups
-
-    return train_clients, train_groups, train_data, test_data
-
-
-if __name__ == '__main__':
-    test = ShakeSpeare(train=True, args=args)
-    x = test.get_client_dic()
-    print(len(x))
-    for i in range(100):
-        print(len(x[i]))
-
-
-dataset_train = ShakeSpeare(train=True, args=args)
-dataset_test = ShakeSpeare(train=False, args=args)
-dict_users = dataset_train.get_client_dic()
-args.num_users = len(dict_users)
-img_size = dataset_train[0][0].shape
-
+if args.dataset == 'cifar':
+    #trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trans_cifar_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    trans_cifar_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    dataset_train = datasets.CIFAR100(args.cifar_dataset, train=True, download=True, transform=trans_cifar_train)
+    dataset_test = datasets.CIFAR100(args.cifar_dataset, train=False, download=True, transform=trans_cifar_test)
+    if args.iid:
+        dict_users = cifar_iid(dataset_train, args.num_users)
+    else:
+        dict_users = cifar_noniid(dataset_train, args.num_users)
 
 import torch
 from torch import nn
 import torch.nn.functional as F
 
-class CharLSTM(nn.Module):
-    def __init__(self):
-        super(CharLSTM, self).__init__()
-        self.embed = nn.Embedding(80, 8)
-        self.lstm = nn.LSTM(8, 256, 2, batch_first=True)
-        # self.h0 = torch.zeros(2, batch_size, 256).requires_grad_()
-        self.drop = nn.Dropout()
-        self.out = nn.Linear(256, 80)
-
+class CIFARLeNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFARLeNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(64 * 8 * 8, 384)  # Assuming input size is 32x32
+        self.fc2 = nn.Linear(384, 192)
+        self.fc3 = nn.Linear(192, num_classes)
+    
     def forward(self, x):
-        x = self.embed(x)
-        # if self.h0.size(1) == x.size(0):
-        #     self.h0.data.zero_()
-        #     # self.c0.data.zero_()
-        # else:
-        #     # resize hidden vars
-        #     device = next(self.parameters()).device
-        #     self.h0 = torch.zeros(2, x.size(0), 256).to(device).requires_grad_()
-        x, hidden = self.lstm(x)
-        x = self.drop(x)
-        # x = x.contiguous().view(-1, 256)
-        # x = x.contiguous().view(-1, 256)
-        return self.out(x[:, -1, :])
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = x.view(-1, 64 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-net_glob = CharLSTM().to(args.device)
+net_glob = CIFARLeNet(args=args).to(args.device)
 print(net_glob)
 
 #training
